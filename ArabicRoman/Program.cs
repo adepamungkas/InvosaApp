64cb63c2dbb046f137f0ada5586735ba1102d89b
adepamungkas <ade.pamungkas9@gmail.com>
2020-03-29 22:34:48 +0700

    Arabic To Roman Number

---
+++ ArabicRoman/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArabicRoman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********** PROGRAM TRANSFORM ARABIC NUMBER TO ROMAN NUMBER ***************:");
+            Console.WriteLine("Enter Arabic Number:");
+            int arabicNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Roman Number is:  {0}",ArabicToRoman(arabicNumber));
+        }
+
+        //Transform Arabic System Number to Roman System Nmmber
+        public static string ArabicToRoman(int arabicNumber)
+        {
+            Dictionary<string, int> pairNumber = new Dictionary<string, int>();
+            
+            pairNumber.Add("M", 1000);
+            pairNumber.Add("CM", 900);
+            pairNumber.Add("D", 500);
+            pairNumber.Add("CD", 400);
+            pairNumber.Add("C", 100);
+            pairNumber.Add("XC", 90);
+            pairNumber.Add("L", 50);
+            pairNumber.Add("XL", 40);
+            pairNumber.Add("X", 10);
+            pairNumber.Add("IX", 9);
+            pairNumber.Add("V", 5);
+            pairNumber.Add("IV", 4);
+            pairNumber.Add("I", 1);
+
+            string result = "";
+
+            foreach (var pair in pairNumber)
+            {
+                while (arabicNumber >= pair.Value)
+                {
+                    arabicNumber -= pair.Value;
+                    result += pair.Key;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Transform/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Transform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********** PROGRAM ENCODING CHARACTERS AND DECODING CHARACTERS ***************:");
+            Console.WriteLine("Choose Menu [1].Encoding  or [2].Decoding: ");
+            int menu = Convert.ToInt32(Console.ReadLine());
+
+            while (!(menu == 1) && !(menu == 2))
+            {
+                Console.WriteLine("Please choose menu [1].Encoding or [2].Decoding !");
+                menu = Convert.ToInt32(Console.ReadLine());
+               
+            }
+
+            if (menu == 1)
+            {
+                Console.WriteLine("Enter the words you want to encode: ");
+                string words = Console.ReadLine();
+                Console.WriteLine("Result Encoding is: {0} ",Encode(words));
+            }
+            else if (menu == 2)
+            {
+                Console.WriteLine("Enter the words you want to decode: ");
+                string words = Console.ReadLine();
+                Console.WriteLine("Result Decoding is: {0} ",Decode(words));
+            }
+
+        }
+
+        //Ecoding character
+        private static string Encode(string words)
+        {
+            string wordsTarget = words.ToUpper();
+
+            Dictionary<Char, Char> charSchema = new Dictionary<Char, Char>();
+            charSchema.Add('@', 'P');
+            charSchema.Add('A', 'Q');
+            charSchema.Add('B', 'R');
+            charSchema.Add('C', 'S');
+            charSchema.Add('D', 'T');
+            charSchema.Add('E', 'U');
+            charSchema.Add('F', 'V');
+            charSchema.Add('G', 'W');
+            charSchema.Add('H', 'X');
+            charSchema.Add('I', 'Y');
+            charSchema.Add('J', 'Z');
+            charSchema.Add('K', '[');
+            charSchema.Add('L', '\\');
+            charSchema.Add('M', ']');
+            charSchema.Add('N', '^');
+            charSchema.Add('O', '_');
+            charSchema.Add('P', '@');
+            charSchema.Add('Q', 'A');
+            charSchema.Add('R', 'B');
+            charSchema.Add('S', 'C');
+            charSchema.Add('T', 'D');
+            charSchema.Add('U', 'E');
+            charSchema.Add('V', 'F');
+            charSchema.Add('W', 'G');
+            charSchema.Add('X', 'H');
+            charSchema.Add('Y', 'I');
+            charSchema.Add('Z', 'J');
+            charSchema.Add('[', 'K');
+            charSchema.Add('\\', 'L');
+            charSchema.Add(']', 'M');
+            charSchema.Add('^', 'N');
+            charSchema.Add('_', 'O');
+
+            string result = "";
+            foreach (char c in wordsTarget)
+            {
+
+                if (charSchema.ContainsKey(c))
+                {
+                    result += charSchema[c];
+                }
+            }
+
+            return result;
+        }
+
+        //decoding character
+        private static string Decode(string words)
+        {
+            string wordsTarget = words.ToUpper();
+
+            Dictionary<Char, Char> charSchema = new Dictionary<Char, Char>();
+            charSchema.Add('P', '@');
+            charSchema.Add('Q', 'A');
+            charSchema.Add('R', 'B');
+            charSchema.Add('S', 'C');
+            charSchema.Add('T', 'D');
+            charSchema.Add('U', 'E');
+            charSchema.Add('V', 'F');
+            charSchema.Add('W', 'G');
+            charSchema.Add('X', 'H');
+            charSchema.Add('Y', 'I');
+            charSchema.Add('Z', 'J');
+            charSchema.Add('[', 'K');
+            charSchema.Add('\\', 'L');
+            charSchema.Add(']', 'M');
+            charSchema.Add('^', 'N');
+            charSchema.Add('_', 'O');
+            charSchema.Add('@', 'P');
+            charSchema.Add('A', 'Q');
+            charSchema.Add('B', 'R');
+            charSchema.Add('C', 'S');
+            charSchema.Add('D', 'T');
+            charSchema.Add('E', 'U');
+            charSchema.Add('F', 'V');
+            charSchema.Add('G', 'W');
+            charSchema.Add('H', 'X');
+            charSchema.Add('I', 'Y');
+            charSchema.Add('J', 'Z');
+            charSchema.Add('K', '[');
+            charSchema.Add('L', '\\');
+            charSchema.Add('M', ']');
+            charSchema.Add('N', '^');
+            charSchema.Add('O', '_');
+
+            string result = "";
+            foreach (char c in wordsTarget)
+            {
+
+                if (charSchema.ContainsKey(c))
+                {
+                    result += charSchema[c];
+                }
+            }
+
+            return result;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
